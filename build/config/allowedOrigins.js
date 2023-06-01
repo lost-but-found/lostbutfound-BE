42e59e95e146d38b4ceb474d3778393d91e953e2
Marvellous Adesanya <victor@nude.club>
2023-06-01 09:09:35 +0100

    Compiled TS

---
+++ build/config/allowedOrigins.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const allowedOrigins = [
+    "https://www.yoursite.com",
+    "http://127.0.0.1:5500",
+    "http://localhost:3500",
+];
+module.exports = allowedOrigins;
+//# sourceMappingURL=allowedOrigins.js.map
---
+++ build/controllers/registerController.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//# sourceMappingURL=registerController.js.map
